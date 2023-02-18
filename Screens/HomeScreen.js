80510162a58132475d17f32b923fe831c8436e4f
Blanche Haddad <blancherachel@gmail.com>
2023-02-18 15:40:21 -0700

    feat: Save list of categories on screen, redirects to doom list

--- Screens/HomeScreen.js
+++ Screens/HomeScreen.js
@@ -1,15 +1,39 @@
-import { StyleSheet, Text, View, FlatList } from "react-native";
+import { StyleSheet, Text, View, FlatList, TouchableOpacity } from "react-native";
 import React from "react";
 
-export default function HomeScreen() {
-  
-  
-  
+export default function HomeScreen({navigation}) {
+  const categories = [
+    { id: 1, type: "Earthquakes" },
+    { id: 2, type: "Volcanoes" },
+    { id: 3, type: "Wildfires" },
+    { id: 4, type: "Severe Storms" },
+  ];
+
+  const pressHandler = () => {
+    navigation.navigate("Doom List")
+  }
+
   return (
     <View>
-      <Text>Chesty</Text>
+      <FlatList
+        data={categories}
+        keyExtractor={(item) => item.id}
+        renderItem={({ item }) => (
+          <TouchableOpacity style={styles.box} onPress={pressHandler}>
+            <Text style={styles.text}>{item.type}</Text>
+          </TouchableOpacity>
+        )}
+      />
     </View>
   );
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  text: {
+    fontSize: 40,
+  },
+  box: {
+    marginVertical: 15,
+    backgroundColor: "blue",
+  },
+});
