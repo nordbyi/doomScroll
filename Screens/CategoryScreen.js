ebcc381f64c2f83d282621b78837abbbe3de11df
Blanche Haddad <blancherachel@gmail.com>
2023-02-18 13:50:39 -0700

    feat: Add View and TExt back in

--- Screens/CategoryScreen.js
+++ Screens/CategoryScreen.js
@@ -1,5 +1,5 @@
 import { StyleSheet, Text, View } from "react-native";
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 export default function CategoryScreen() {
   return (
