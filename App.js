c6498ff7d5553c9c18ae7cc7a57cf04bdba4c4bc
Blanche Haddad <blancherachel@gmail.com>
2023-02-18 15:39:38 -0700

    feat: Rename one screen

--- App.js
+++ App.js
@@ -2,31 +2,36 @@ import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
-import HomeScreen from "./Screens/HomeScreen";
 import { SafeAreaProvider } from "react-native-safe-area-context";
+import { useEffect, useState } from "react";
+import { fetchEarthquakeData, fetchDisasterData} from "./ApiCalls/apiCalls";
+import HomeScreen from "./Screens/HomeScreen";
 import CategoryScreen from "./Screens/CategoryScreen";
 import SearchScreen from "./Screens/SearchScreen";
 import FilteredScreen from "./Screens/FilteredScreen";
 import DisasterDetailsScreen from "./Screens/DisasterDetailsScreen";
-import { useEffect, useState } from "react";
-import fetchEarthquakeData from "./ApiCalls/apiCalls";
 
 const Stack = createNativeStackNavigator();
 
-
-
 export default function App() {
 
+  const [earthquakeData, setEarthquakeData] = useState([]);
+  const [disasterData, setDisasterData] = useState([]);
+  
   useEffect(() => {
-    console.log(fetchEarthquakeData());
-  }, [])
+    fetchEarthquakeData().then(res => res.json()).then(data => setEarthquakeData(data.data[0]));
+    fetchDisasterData("wildfires").then(res => res.json()).then(data => console.log(data.events[0]));
+    fetchDisasterData("severeStorms").then(res => res.json()).then(data => console.log(data.events[0]));
+    fetchDisasterData("volcanoes").then(res => res.json()).then(data => console.log(data.events[0]));
+  }, []);
+ 
 
   return (
-    <SafeAreaProvider>?
+    <SafeAreaProvider>
       <NavigationContainer>
         <Stack.Navigator initialRouteName="Home">
           <Stack.Screen name="DoomScroll" component={HomeScreen} />
-          <Stack.Screen name="Doom Categories" component={CategoryScreen} />
+          <Stack.Screen name="Doom List" component={CategoryScreen} />
           <Stack.Screen name="Doom Details" component={DisasterDetailsScreen} />
           <Stack.Screen name="Search for Doom" component={SearchScreen} />
           <Stack.Screen name="Meet Your Doom" component={FilteredScreen} />
