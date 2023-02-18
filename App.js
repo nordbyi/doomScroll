2788369802fb22995298cbe10c1797c19d72b545
Blanche Haddad <blancherachel@gmail.com>
2023-02-18 11:56:45 -0700

    feat: Add navigation for all screens

--- App.js
+++ App.js
@@ -1,18 +1,32 @@
 import { StatusBar } from "expo-status-bar";
-import { SafeAreaView, StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import HomeScreen from "./Screens/HomeScreen";
+import { SafeAreaProvider } from "react-native-safe-area-context";
+import CategoryScreen from "./Screens/CategoryScreen";
+import SearchScreen from "./Screens/SearchScreen";
+import FilteredScreen from "./Screens/FilteredScreen";
+import DisasterDetailsScreen from "./Screens/DisasterDetailsScreen";
 
 const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
-    <NavigationContainer>
-      <Stack.Navigator initialRouteName="Home">
-        <Stack.Screen name="Home" component={HomeScreen} />
-      </Stack.Navigator>
-    </NavigationContainer>
+    <SafeAreaProvider>
+      <NavigationContainer>
+        <Stack.Navigator initialRouteName="Home">
+          <Stack.Screen name="DoomScroll" component={HomeScreen} />
+          <Stack.Screen name="Disaster Categories" component={CategoryScreen} />
+          <Stack.Screen
+            name="Your Disaster"
+            component={DisasterDetailsScreen}
+          />
+          <Stack.Screen name="Search" component={SearchScreen} />
+          <Stack.Screen name="Search Results" component={FilteredScreen} />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </SafeAreaProvider>
   );
 }
 
