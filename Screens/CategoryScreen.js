23a1c6e0f65669576f3ec8965e2c0c99ceac3481
Blanche Haddad <blancherachel@gmail.com>
2023-02-18 15:39:56 -0700

    feat: Save work

--- Screens/CategoryScreen.js
+++ Screens/CategoryScreen.js
@@ -1,7 +1,16 @@
 import { StyleSheet, Text, View } from "react-native";
 import React, { useEffect, useState } from "react";
+import { fetchEarthquakeData, fetchDisasterData } from "../ApiCalls/apiCalls";
 
 export default function CategoryScreen() {
+  const [earthquakeData, setEarthquakeData] = useState([]);
+  const [disasterData, setDisasterData] = useState([]);
+  
+  useEffect(() => {
+    fetchEarthquakeData().then(res => res.json()).then(data => setEarthquakeData(data.data));
+    fetchDisasterData("wildfires").then(res => res.json()).then(data => setDisasterData(data));
+  }, []);
+
   return (
     <View>
       <Text>GET ON BOARD IAN</Text>
