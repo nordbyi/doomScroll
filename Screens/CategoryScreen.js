d278ee808415e0165efda6b575174383183b7dd9
Blanche Haddad <blancherachel@gmail.com>
2023-02-18 17:07:14 -0700

    feat: Add route.params for sending the endpoint to this component

--- Screens/CategoryScreen.js
+++ Screens/CategoryScreen.js
@@ -2,14 +2,23 @@ import { StyleSheet, Text, View } from "react-native";
 import React, { useEffect, useState } from "react";
 import { fetchEarthquakeData, fetchDisasterData } from "../ApiCalls/apiCalls";
 
-export default function CategoryScreen() {
+export default function CategoryScreen({ route }) {
+  console.log(route)
   const [earthquakeData, setEarthquakeData] = useState([]);
   const [disasterData, setDisasterData] = useState([]);
+  const [error, setError] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
   
   useEffect(() => {
-    fetchEarthquakeData().then(res => res.json()).then(data => setEarthquakeData(data.data));
-    fetchDisasterData("wildfires").then(res => res.json()).then(data => setDisasterData(data));
+    if (route.params === "earthquakes") {
+    fetchEarthquakeData().then(res => res.json()).then(data => setEarthquakeData(data.data)).catch(err => setError(err));
+    } else {
+      fetchDisasterData(route.params).then(res => res.json()).then(data => setDisasterData(data.events)).catch(err => setError(err));
+    }
   }, []);
+  console.log("earthquake bithc", earthquakeData);
+  console.log("disaster babyyyy", disasterData);
+  console.log("eroroeiwshfeuow", error);
 
   return (
     <View>
