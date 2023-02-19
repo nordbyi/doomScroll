4d77892f2a44809676c023c19dabc3f1dc42e5dd
Angie Staffieri <angie.staffieri@gmail.com>
2023-02-19 11:06:41 -0700

    mapping data

--- Screens/CategoryScreen.js
+++ Screens/CategoryScreen.js
@@ -1,4 +1,4 @@
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View, FlatList } from "react-native";
 import React, { useEffect, useState } from "react";
 import { fetchEarthquakeData, fetchDisasterData } from "../ApiCalls/apiCalls";
 
@@ -11,18 +11,44 @@ export default function CategoryScreen({ route }) {
   
   useEffect(() => {
     if (route.params === "earthquakes") {
-    fetchEarthquakeData().then(res => res.json()).then(data => setEarthquakeData(data.data)).catch(err => setError(err));
+    fetchEarthquakeData().then(res => res.json()).then(data => {
+      const mappedEarthquake = data.map(data => {
+        return {
+          id: data.data.id,
+          title: data.data.title,
+          date: data.data.date,
+          location: data.data.location,
+          magnitude: data.data.magnitude,
+          source: data.data.url
+        }
+      })
+    setEarthquakeData(data.data)
+  }).catch(err => setError(err));
     } else {
-      fetchDisasterData(route.params).then(res => res.json()).then(data => setDisasterData(data.events)).catch(err => setError(err));
+      fetchDisasterData(route.params).then(res => res.json()).then(data => {
+        const mappedData = data.map(data => {
+         return {
+          id: data.events.id,
+          title: data.events.title,
+          coordinates: data.events.geometry,
+          source: data.events.sources.url
+        }
+        })
+        setDisasterData(mappedData)
+      }).catch(err => setError(err));
     }
   }, []);
-  console.log("earthquake bithc", earthquakeData);
+  // console.log("earthquake bithc", earthquakeData);
   console.log("disaster babyyyy", disasterData);
   console.log("eroroeiwshfeuow", error);
 
   return (
     <View>
-      <Text>GET ON BOARD IAN</Text>
+      <FlatList 
+        data={disasterData}
+      >
+
+      </FlatList>
     </View>
   );
 }
