2ed3c915d0aff52fbe6e42e70f5e239d42d27c2a
Josephine Heidepriem <jheidepriem@gmail.com>
2023-02-21 19:51:58 -0600

    Delete SearchForm

--- App.js
+++ App.js
@@ -7,24 +7,12 @@ import { useEffect, useState } from "react";
 import { fetchEarthquakeData, fetchDisasterData} from "./ApiCalls/apiCalls";
 import HomeScreen from "./Screens/HomeScreen";
 import CategoryScreen from "./Screens/CategoryScreen";
-import SearchScreen from "./Screens/SearchScreen";
 import FilteredScreen from "./Screens/FilteredScreen";
 import DisasterDetailsScreen from "./Screens/DisasterDetailsScreen";
 
 const Stack = createNativeStackNavigator();
 
 export default function App() {
-
-//   const [earthquakeData, setEarthquakeData] = useState([]);
-//   const [disasterData, setDisasterData] = useState([]);
-  
-//   useEffect(() => {
-//     fetchEarthquakeData().then(res => res.json()).then(data => setEarthquakeData(data.data[0]));
-//     fetchDisasterData("wildfires").then(res => res.json()).then(data => console.log(data.events[0]));
-//     fetchDisasterData("severeStorms").then(res => res.json()).then(data => console.log(data.events[0]));
-//     fetchDisasterData("volcanoes").then(res => res.json()).then(data => console.log(data.events[0]));
-//   }, []);
-//  console.log("earthquaaaake", earthquakeData);
 
   return (
     <SafeAreaProvider>
@@ -33,7 +21,6 @@ export default function App() {
           <Stack.Screen name="DoomScroll" component={HomeScreen} />
           <Stack.Screen name="Doom List" component={CategoryScreen} />
           <Stack.Screen name="Doom Details" component={DisasterDetailsScreen} />
-          <Stack.Screen name="Search for Doom" component={SearchScreen} />
           <Stack.Screen name="Meet Your Doom" component={FilteredScreen} />
         </Stack.Navigator>
       </NavigationContainer>
