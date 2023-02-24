d90873b44c723f8df158a62d5026894341ae6957
Josephine Heidepriem <jheidepriem@gmail.com>
2023-02-24 17:26:58 -0600

    Add cypress dependencies

--- cypress/e2e/HomeScreen.cy.js
+++ cypress/e2e/HomeScreen.cy.js
@@ -1,6 +1,10 @@
 describe('Home Screen', () => {
   it('Should have a list of categories', () => {
-    cy.request('http://localhost:19000/')
-    cy.get()
+    cy.request('http://localhost:19006/') //beforeEach broke our test
+    cy.contains("Earthquakes");
+    cy.contains("Volcanoes");
+    cy.contains("Wildfires");
+    cy.contains("Severe Storms");
+    cy.contains("Asteroids");
   })
 })
