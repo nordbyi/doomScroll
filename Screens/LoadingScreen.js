409f8b428bcd9b1456a07bece88961ead94b4f44
Blanche Haddad <blancherachel@gmail.com>
2023-03-10 09:14:28 -0700

    fix: Remove logo from loading screen

--- Screens/LoadingScreen.js
+++ Screens/LoadingScreen.js
@@ -39,12 +39,6 @@ const LoadingScreen = () => {
         source={require("../assets/splash.png")}
       ></Image>
     </View>
-    <View> 
-      <Image
-        style={styles.logo}
-        source={require("../assets/logo.png")}
-      ></Image>
-    </View>
   </View>
   );
 };
