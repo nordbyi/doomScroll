a58e5a2d167f48f5562f27ff002dc36b284623a8
Ian Nordby <ian.c.nordby@gmail.com>
2023-03-10 14:31:03 -0700

    Import and render planet orbits

--- Screens/AsteroidSpinnerScreen.js
+++ Screens/AsteroidSpinnerScreen.js
@@ -1,7 +1,23 @@
 import React from "react";
 import { Animated, StyleSheet, View, Image, Easing } from "react-native";
+import Sun from "./orbits/Sun";
+import Mercury from "./orbits/Mercury";
+import Venus from "./orbits/Venus";
+import Earth from "./orbits/Earth";
+import Mars from "./orbits/Mars";
+import Jupiter from "./orbits/Jupiter";
 
 const AsteroidSpinnerScreen = () => {
+  const spinValue = new Animated.Value(0);
+
+  Animated.loop(
+    Animated.timing(spinValue, {
+      toValue: 1,
+      duration: 5000,
+      easing: Easing.linear, // Easing is an additional import from react-native
+      useNativeDriver: true, // To make use of native driver for performance
+    })
+  ).start();
 
   const spin = spinValue.interpolate({
     inputRange: [0, 1],
@@ -9,12 +25,22 @@ const AsteroidSpinnerScreen = () => {
   });
   return (
     <View style={styles.screen}>
-      <View style={styles.orbitMercury}>
+      <Sun />
+      {/* <Animated.View style={[styles.orbitMercury, {
+            transform: [{ rotate: spin }],
+          },]}>
         <View style={styles.mercury} />
-      </View>
-      <View style={styles.orbitVenus}>
+      </Animated.View> */}
+      <Mercury />
+      {/* <Animated.View style={[styles.orbitVenus, {
+            transform: [{ rotate: spin }],
+          }]}>
         <View style={styles.venus} />
-      </View>
+      </Animated.View> */}
+      <Venus />
+      <Earth />
+      <Mars />
+      <Jupiter />
     </View>
   );
 };
@@ -23,6 +49,7 @@ export default AsteroidSpinnerScreen;
 
 const styles = StyleSheet.create({
   screen: {
+    position: 'relative',
     backgroundColor: "#020d19",
     height: "100%",
     justifyContent: "center",
