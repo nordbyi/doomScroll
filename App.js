ea22afc903483f7b6adfe3085b57007e415b4206
Ian Nordby <ian.c.nordby@gmail.com>
2023-03-10 11:42:09 -0700

    Add temporary screen view for AsteroidSpinner

--- App.js
+++ App.js
@@ -5,19 +5,50 @@ import HomeScreen from "./Screens/HomeScreen";
 import CategoryScreen from "./Screens/CategoryScreen";
 import DisasterDetailsScreen from "./Screens/DisasterDetailsScreen";
 import AboutUsScreen from "./Screens/AboutUsScreen";
+import AsteroidSpinnerScreen from "./Screens/AsteroidSpinnerScreen";
+
+// type 'expo r -c' in terminal to run
+// or 'npx expo start'
 
 const Stack = createNativeStackNavigator();
 
 export default function App() {
-
   return (
     <SafeAreaProvider>
       <NavigationContainer>
-        <Stack.Navigator initialRouteName="Home" screenOptions={{headerStyle: {backgroundColor: "#020d19"}, headerTitleStyle: {color: "#e7e5d7", fontSize: 20}, headerBackTitle: "Back"}} >
-          <Stack.Screen name="Doom Scroll" component={HomeScreen} screenOptions={{headerBackTitle: "Back"}} />
-          <Stack.Screen name="Doom List" component={CategoryScreen} screenOptions={{headerBackTitle: "Back"}} />
-          <Stack.Screen name="Doom Details" component={DisasterDetailsScreen} screenOptions={{headerBackTitle: "Back"}} />
-          <Stack.Screen name="About Us" component={AboutUsScreen} screenOptions={{headerBackTitle: "Back"}} />
+        <Stack.Navigator
+          initialRouteName="Home"
+          screenOptions={{
+            headerStyle: { backgroundColor: "#020d19" },
+            headerTitleStyle: { color: "#e7e5d7", fontSize: 20 },
+            headerBackTitle: "Back",
+          }}
+        >
+          <Stack.Screen
+            name="Doom Scroll"
+            component={HomeScreen}
+            screenOptions={{ headerBackTitle: "Back" }}
+          />
+          <Stack.Screen
+            name="Doom List"
+            component={CategoryScreen}
+            screenOptions={{ headerBackTitle: "Back" }}
+          />
+          <Stack.Screen
+            name="Doom Details"
+            component={DisasterDetailsScreen}
+            screenOptions={{ headerBackTitle: "Back" }}
+          />
+          <Stack.Screen
+            name="About Us"
+            component={AboutUsScreen}
+            screenOptions={{ headerBackTitle: "Back" }}
+          />
+          <Stack.Screen
+            name="Spinner"
+            component={AsteroidSpinnerScreen}
+            screenOptions={{ headerBackTitle: "Back" }}
+          />
         </Stack.Navigator>
       </NavigationContainer>
     </SafeAreaProvider>
