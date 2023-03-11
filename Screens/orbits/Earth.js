ac2ea5105ad570064fea112f7740bbf69a9797be
Ian Nordby <ian.c.nordby@gmail.com>
2023-03-11 10:56:31 -0700

    Add code to spin orbit back to starting position and change border color to red when pressed

--- Screens/orbits/Earth.js
+++ Screens/orbits/Earth.js
@@ -1,40 +1,79 @@
-import React from "react";
-import { Animated, StyleSheet, View, Image, Easing } from "react-native";
+import React, {useState, useRef, useEffect} from "react";
+import {
+  Animated,
+  StyleSheet,
+  View,
+  Easing,
+  Pressable,
+} from "react-native";
 
 const Earth = () => {
-  const spinValue = new Animated.Value(0);
+  const [color, setColor] = useState("#e7e5d7")
 
-  Animated.loop(
+  useEffect(() => {
+    orbit.start()
+  }, [])
+
+  const spinValue = useRef(new Animated.Value(0)).current;
+
+  const spinToTop = Animated.timing(spinValue, {
+    toValue: 0,
+    duration: 500,
+    easing: Easing.linear,
+    useNativeDriver: true,
+  });
+
+  const orbit = Animated.loop(
     Animated.timing(spinValue, {
       toValue: 1,
       duration: 10000,
       easing: Easing.linear, // Easing is an additional import from react-native
       useNativeDriver: true, // To make use of native driver for performance
     })
-  ).start();
+  );
 
   const spin = spinValue.interpolate({
     inputRange: [0, 1],
     outputRange: ["0deg", "360deg"],
   });
+
   return (
-      <Animated.View style={[styles.orbitEarth, {
+    <Pressable style={styles.pressable} onPressIn={() => {
+      setColor('red')
+      orbit.stop()
+      spinToTop.start()}
+    } onPressOut ={() => {
+      setColor('#e7e5d7')
+      orbit.start()
+    }
+    }>
+      <Animated.View
+        
+        style={[
+          {borderColor: color},
+          styles.orbitEarth,
+          {
             transform: [{ rotate: spin }],
-          },]}>
+          },
+        ]}
+      >
         <View style={styles.earth} />
       </Animated.View>
+    </Pressable>
   );
 };
 
 export default Earth;
 
 const styles = StyleSheet.create({
+  pressable: {
+    width: 300,
+    height: 300,
+  },
   orbitEarth: {
     position: "absolute",
     width: 300,
     height: 300,
-    // borderStyle: "dashed",
-    borderColor: "#e7e5d7",
     borderWidth: 2,
     borderRadius: 150,
   },
