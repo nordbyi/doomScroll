2b9c957602d551bae333a9719c5c89e1ca6f751d
Ian Nordby <ian.c.nordby@gmail.com>
2023-03-12 15:25:13 -0600

    Add animations to expand planet size for displaying content when orbit is pressed

--- Screens/orbits/Mars.js
+++ Screens/orbits/Mars.js
@@ -1,13 +1,66 @@
-import React, {useState, useEffect, useRef} from "react";
-import { Animated, StyleSheet, View, Pressable } from "react-native";
+import React, { useState, useEffect, useRef } from "react";
+import { Animated, StyleSheet, View, Pressable, Easing } from "react-native";
 import { orbit, spin, spinToTop } from "./helperFunctions";
 
 const Mars = () => {
   const [color, setColor] = useState("#e7e5d7");
+  // const [stylings, setStylings] = useState({
+  //   top: -9,
+  //   left: 215,
+  //   backgroundColor: "#e7e5d7",
+  //   width: 18,
+  //   height: 18,
+  //   borderRadius: 9,
+  // }); // use these stlyings as default.  Update them when touching orbit to expand planet to fill screen
 
   useEffect(() => {
     marsOrbit.start();
   }, []);
+
+  // const widthValue = new Animated.Value(18)
+  const widthValue = useRef(new Animated.Value(18)).current;
+  const heightValue = useRef(new Animated.Value(18)).current;
+  // const leftValue = useRef(new Animated.Value(215)).current;
+
+
+  const expand = Animated.timing(widthValue, {
+    toValue: 390,
+    duration: 600,
+    easing: Easing.linear, 
+    useNativeDriver: false,
+  })
+
+  const contract = Animated.timing(widthValue, {
+    toValue: 18,
+    duration: 600,
+    easing: Easing.linear,
+    useNativeDriver: false,
+  })
+
+  const expandHeight = Animated.timing(heightValue, {
+    toValue: 400,
+    duration: 600,
+    easing: Easing.linear, 
+    useNativeDriver: false,
+  })
+  const contractHeight = Animated.timing(heightValue, {
+    toValue: 18,
+    duration: 600,
+    easing: Easing.linear,
+    useNativeDriver: false,
+  })
+  // const expandLeft = Animated.timing(leftValue, {
+  //   toValue: 0,
+  //   duration: 1000,
+  //   easing: Easing.linear, 
+  //   useNativeDriver: false,
+  // })
+  // const contractLeft = Animated.timing(leftValue, {
+  //   toValue: 215,
+  //   duration: 1000,
+  //   easing: Easing.linear,
+  //   useNativeDriver: false,
+  // })
 
   const spinValue = useRef(new Animated.Value(0)).current;
 
@@ -17,6 +70,21 @@ const Mars = () => {
 
   const marsSpin = spin(spinValue);
 
+  // const expandPlanet = () => {
+  //   setTimeout(() => {
+  //     // Animated.timing(widthValue, {
+  //     //   toValue: 200
+  //     // }).start()
+  //   setStylings({
+  //     top: 0,
+  //     left: 0,
+  //     backgroundColor: "#e7e5d7",
+  //     width: 100,
+  //     height: 400,
+  //     borderRadius: 5,
+  //   })}, 700)
+  // };
+
   return (
     <Pressable
       style={styles.pressable}
@@ -24,10 +92,17 @@ const Mars = () => {
         setColor("red");
         marsOrbit.stop();
         marsSpinToTop.start();
+        // expandPlanet()
+        expand.start()
+        expandHeight.start()
+        // expandLeft.start()
       }}
       onPressOut={() => {
         setColor("#e7e5d7");
         marsOrbit.start();
+        contract.start()
+        contractHeight.start()
+        // contractLeft.start()
       }}
     >
       <Animated.View
@@ -39,7 +114,20 @@ const Mars = () => {
           },
         ]}
       >
-        <View style={styles.mars} />
+        <Animated.View
+          style={[
+            styles.mars,
+            { width: widthValue },
+            { height: heightValue },
+            // { left: leftValue }
+            // { top: stylings.top },
+            // { left: stylings.left },
+            // { backgroundColor: stylings.backgroundColor },
+            // { width: stylings.width },
+            // { height: stylings.height },
+            // { borderRadius: stylings.borderRadius },
+          ]}
+        />
       </Animated.View>
     </Pressable>
   );
@@ -61,14 +149,15 @@ const styles = StyleSheet.create({
     // borderColor: "#e7e5d7",
     borderWidth: 2,
     borderRadius: 225,
+    alignItems: 'center'
   },
   mars: {
     position: "absolute",
     top: -9,
-    left: 215,
+    // left: 215,
     backgroundColor: "#e7e5d7",
-    width: 18,
-    height: 18,
+    // width: 18,
+    // height: 18 ,
     borderRadius: 9,
   },
 });
