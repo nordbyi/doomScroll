4c81a52c04bad8398b652785ee1ad909cc315ae1
Ian Nordby <ian.c.nordby@gmail.com>
2023-03-13 17:50:08 -0600

    Add Countdown component to display a countown to doomsday event for a planet

---
+++ Screens/orbits/Countdown.js
@@ -0,0 +1,20 @@
+import React from "react";
+import { StyleSheet, View, Text } from "react-native";
+
+const Countdown = () => {
+  return (
+    <View>
+      <Text>Hello</Text>
+    </View>
+  );
+};
+
+export default Countdown;
+
+const styles = StyleSheet.create({
+  pressable: {
+    position: "absolute",
+    width: 450,
+    height: 450,
+  },
+});
