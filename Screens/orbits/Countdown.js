4a5c5d2675119e068226a3108616e5d6f00ea297
Ian Nordby <ian.c.nordby@gmail.com>
2023-03-15 10:08:41 -0600

    Add a countdown timer and render it

--- Screens/orbits/Countdown.js
+++ Screens/orbits/Countdown.js
@@ -1,10 +1,20 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 import { StyleSheet, View, Text } from "react-native";
 
-const Countdown = () => {
+const Countdown = ({yearsUntilEvent, fact}) => {
+  const [countdown, setCountdown] = useState(yearsUntilEvent * 365 * 24 * 60 * 60) // calculate time left until doom event
+
+  useEffect(() => {
+    const timerID = setTimeout(() => setCountdown(countdown - 1), 1000);
+    return () => clearTimeout(timerID)
+  }, [countdown])
+
   return (
-    <View>
-      <Text>Hello</Text>
+    <View style={styles.container}>
+      <Text style={styles.fact}>{fact}</Text>
+      <Text style={styles.fact}>The same DOOM will befall Earth in...</Text>
+      {yearsUntilEvent > 100 && <Text style={styles.countdown}>409,968,753,21</Text>}
+      <Text style={styles.countdown}>{countdown.toLocaleString()}</Text>
     </View>
   );
 };
@@ -12,9 +22,22 @@ const Countdown = () => {
 export default Countdown;
 
 const styles = StyleSheet.create({
-  pressable: {
-    position: "absolute",
-    width: 450,
-    height: 450,
+  container: {
+    width: "100%",
+    height: "100%",
+    alignItems: 'center',
+    backgroundColor:  "#e7e5d7",
+    borderRadius: 100
   },
+  countdown: {
+    fontSize: 35,
+    flexShrink: 1,
+    color: 'red'
+  },
+  fact: {
+    padding: '5%',
+    fontSize: 28,
+    flexShrink: 1,
+    color: "#1e2f42"
+  }
 });
