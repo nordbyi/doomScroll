f9ce6096da4b0ab560a2dc59d2248142130d7486
Ian Nordby <ian.c.nordby@gmail.com>
2023-03-20 17:05:48 -0600

    Replace individual planets with reusable Planet component

--- Screens/AsteroidSpinnerScreen.js
+++ Screens/AsteroidSpinnerScreen.js
@@ -6,17 +6,28 @@ import Venus from "./orbits/Venus";
 import Earth from "./orbits/Earth";
 import Mars from "./orbits/Mars";
 import Jupiter from "./orbits/Jupiter";
+import Planet from "./orbits/Planet";
 
 const AsteroidSpinnerScreen = () => {
-
+  const planetFacts = {
+    mars: "Roughly four billion years ago, Mars' core solidified, its magnetic field disappeared, and the solar wind stripped its atmosphere away.",
+    venus: "Venus' atmosphere filled with carbon dioxide causing a runaway greenhouse effect. It's the hottest planet in our solar system, even though Mercury is closer to the Sun."
+    // mercury:
+  }
   return (
     <View style={styles.screen}>
       <Sun />
-      <Jupiter />
-      <Mars />
+      {/* <Jupiter /> */}
+      <Planet name={'Jupiter'} containerSize={720} planetSize={34} orbitPeriod={55000} yearsUntilEvent={10} fact={'Testing 1 2 1 2'}/>
+      <Planet name={'Mars'} containerSize={450} planetSize={18} orbitPeriod={15000} yearsUntilEvent={301} fact={planetFacts.mars}/>
+      <Planet name={'Earth'} containerSize={300} planetSize={22} orbitPeriod={10000} yearsUntilEvent={10} fact={'Testing 1 2 1 2'}/>
+      <Planet name={'Venus'} containerSize={180} planetSize={20} orbitPeriod={5000} yearsUntilEvent={20} fact={planetFacts.venus}/>
+      <Planet name={'Jupiter'} containerSize={100} planetSize={12} orbitPeriod={4000} yearsUntilEvent={10} fact={'Testing 1 2 1 2'}/>
+      
+      {/* <Mars />
       <Earth />
       <Venus />
-      <Mercury /> 
+      <Mercury />  */}
     </View>
   );
 };
